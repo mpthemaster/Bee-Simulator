545fd2c534ee56db71932ed0e32e62b15b4d6d1b
Michael Petruzzello <Tedybear3@aol.com>
2014-05-29 16:53:06 -0400

    Printing functionality added.

--- Bee Simulator/Form1.cs
+++ Bee Simulator/Form1.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Drawing;
+using System.Drawing.Printing;
 using System.IO;
 using System.Linq;
 using System.Runtime.Serialization.Formatters.Binary;
@@ -207,6 +208,105 @@ namespace Bee_Simulator
         private void timer2_Tick(object sender, EventArgs e)
         {
             renderer.AnimateBees();
+        }
+
+        private void printToolStripButton_Click(object sender, EventArgs e)
+        {
+            bool simRunning = timer1.Enabled;
+
+            if (simRunning)
+                timer1.Stop();
+
+            PrintDocument document = new PrintDocument();
+            document.PrintPage += document_PrintPage;
+            PrintPreviewDialog preview = new PrintPreviewDialog();
+            preview.Document = document;
+            preview.ShowDialog(this);
+
+            if (simRunning)
+                timer1.Start();
+        }
+
+        private void document_PrintPage(object sender, PrintPageEventArgs e)
+        {
+            Graphics g = e.Graphics;
+            Size stringSize;
+
+            //Thee Bee Simulator oval and text.
+            using (Font arial24bold = new Font("Arial", 24, FontStyle.Bold))
+            {
+                stringSize = Size.Ceiling(g.MeasureString("Bee Simulator", arial24bold));
+
+                g.FillEllipse(Brushes.Gray, new Rectangle(e.MarginBounds.X + 2, e.MarginBounds.Y + 2, stringSize.Width + 30, stringSize.Height + 30));
+                g.FillEllipse(Brushes.Black, new Rectangle(e.MarginBounds.X, e.MarginBounds.Y, stringSize.Width + 30, stringSize.Height + 30));
+                g.DrawString("Bee Simulator", arial24bold, Brushes.Gray, e.MarginBounds.X + 17, e.MarginBounds.Y + 17);
+                g.DrawString("Bee Simulator", arial24bold, Brushes.White, e.MarginBounds.X + 15, e.MarginBounds.Y + 15);
+            }
+
+            int tableX = e.MarginBounds.X + (int)stringSize.Width + 50;
+            int tableWidth = e.MarginBounds.X + e.MarginBounds.Width - tableX - 20;
+            int firstColumnX = tableX + 2;
+            int secondColumnX = tableX + tableWidth / 2 + 5;
+            int tableY = e.MarginBounds.Y;
+
+            //The table's borders.
+            g.DrawLine(Pens.Black, firstColumnX, tableY, firstColumnX, tableY + 144);
+            g.DrawLine(Pens.Black, secondColumnX, tableY, secondColumnX, tableY + 144);
+            g.DrawLine(Pens.Black, tableX + tableWidth + 2, tableY, tableX + tableWidth + 2, tableY + 144);
+            g.DrawLine(Pens.Black, firstColumnX, tableY, firstColumnX + tableWidth, tableY);
+
+            //The table's data within.
+            tableY = PrintTableRow(g, tableX, tableWidth, firstColumnX, secondColumnX, tableY, "Bees", world.Bees.Count.ToString());
+            tableY = PrintTableRow(g, tableX, tableWidth, firstColumnX, secondColumnX, tableY, "Flowers", world.Flowers.Count.ToString());
+            tableY = PrintTableRow(g, tableX, tableWidth, firstColumnX, secondColumnX, tableY, "Honey in Hive", String.Format("{0:f3}", world.Hive.Honey));
+            tableY = PrintTableRow(g, tableX, tableWidth, firstColumnX, secondColumnX, tableY, "Nectar in Flowers", lblNectarInFlowers.Text);
+            tableY = PrintTableRow(g, tableX, tableWidth, firstColumnX, secondColumnX, tableY, "Frames Run", lblFramesRun.Text);
+            PrintTableRow(g, tableX, tableWidth, firstColumnX, secondColumnX, tableY, "Frame Rate", lblFrameRate.Text);
+
+            //The Hive and Field pictures.
+            using (Pen blackPen = new Pen(Brushes.Black, 2))
+            using (Bitmap hiveBitmap = new Bitmap(hiveForm.ClientSize.Width, hiveForm.ClientSize.Height))
+            using (Bitmap fieldBitmap = new Bitmap(fieldForm.ClientSize.Width, fieldForm.ClientSize.Height))
+            {
+                using (Graphics hiveGraphics = Graphics.FromImage(hiveBitmap))
+                    renderer.PaintHive(hiveGraphics);
+
+                int hiveWidth = e.MarginBounds.Width / 2;
+                float ratio = (float)hiveBitmap.Height / (float)hiveBitmap.Width;
+                int hiveHeight = (int)(hiveWidth * ratio);
+                int hiveX = e.MarginBounds.X + (e.MarginBounds.Width - hiveWidth) / 2;
+                int hiveY = e.MarginBounds.Height / 3;
+
+                g.DrawImage(hiveBitmap, hiveX, hiveY, hiveWidth, hiveHeight);
+                g.DrawRectangle(blackPen, hiveX, hiveY, hiveWidth, hiveHeight);
+
+                using (Graphics fieldGraphics = Graphics.FromImage(fieldBitmap))
+                    renderer.PaintField(fieldGraphics);
+
+                int fieldWidth = e.MarginBounds.Width;
+                ratio = (float)fieldBitmap.Height / (float)fieldBitmap.Width;
+                int fieldHeight = (int)(fieldWidth * ratio);
+                int fieldX = e.MarginBounds.X;
+                int fieldY = e.MarginBounds.Y + e.MarginBounds.Height - fieldHeight;
+
+                g.DrawImage(fieldBitmap, fieldX, fieldY, fieldWidth, fieldHeight);
+                g.DrawRectangle(blackPen, fieldX, fieldY, fieldWidth, fieldHeight);
+            }
+        }
+
+        private int PrintTableRow(Graphics printGraphics, int tableX, int tableWidth, int firstColumnX, int secondColumnX, int tableY, string firstColumn, string secondColumn)
+        {
+            Font arial12 = new Font("Arial", 12);
+            Size stringSize = Size.Ceiling(printGraphics.MeasureString(firstColumn, arial12));
+            tableX += 2;
+
+            printGraphics.DrawString(firstColumn, arial12, Brushes.Black, firstColumnX, tableY);
+            printGraphics.DrawString(secondColumn, arial12, Brushes.Black, secondColumnX, tableY);
+
+            tableY += (int)stringSize.Height + 2;
+            printGraphics.DrawLine(Pens.Black, tableX, tableY, tableX + tableWidth, tableY);
+            arial12.Dispose();
+            return tableY;
         }
     }
 }
